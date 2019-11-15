5c0c8bf2505eaa00e31b70a3e48aa4423cc8a7b7
Brian P. Anderson <bpa@notionovus.com>
2019-11-15 03:52:01 -0600

    Ugh, spaces

--- meadowlark.js
+++ meadowlark.js
@@ -6,17 +6,17 @@ app.set('port', process.env.PORT || 3000);
 
 // custom 404 page
 app.use(function(req, res){
-        res.type('text/plain');
-        res.status(404);
-        res.send('404 - Not Found');
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next){
-        console.error(err.stack);
-        res.type('text/plain');
-        res.status(500);
-        res.send('500 - Server Error');
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
 });
 
 app.listen(app.get('port'), function(){
